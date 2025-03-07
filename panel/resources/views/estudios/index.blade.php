2252404c9757505cd2f6ab4070fac6d61d522579
Daenloye <ldanielenrique@gmail.com>
2025-03-07 15:25:13 -0500

    Se ajusta el listado de estudios

--- panel/resources/views/estudios/index.blade.php
+++ panel/resources/views/estudios/index.blade.php
@@ -1,2 +1,66 @@
 @extends('paneltemplate')
 @section('title','Listado de estudios')
+
+@section("contenido")
+    <div class="card shadow-custom">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-9">
+                    <h5 class="card-title">Listado de estudios</h5>
+                    {{-- <p class="card-text">With supporting text below as a natural lead-in to additional content.</p> --}}
+                </div>
+                <div class="col-md-3 justify-content-end" style="display: flex; gap:0.6rem">
+                    <button type="button" class="btn btn-sm action-btn btn-outline-secondary">
+                        <i class="fa-solid fa-plus"></i> Crear
+                    </button>
+                    
+                    <button type="button" class="btn btn-sm action-btn btn-outline-secondary">
+                        <i class="fa-solid fa-filter"></i> Filtrar
+                    </button>
+                    
+                </div>
+                <div class="col-md-12">
+                    Filtros
+                </div>
+                <div class="col-md-12">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">#id</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Ciudad</th>
+                            <th scope="col">Modelos</th>
+                            <th scope="col">Acciones</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>
+                                <a type="button" class="btn btn-outline-secondary btn-sm" href="modelos.php?id=">Ver</a>
+                                <a type="button" class="btn btn-outline-primary btn-sm" href="editarestudio.php?id=">Editar</a>
+                                <button type="button" class="btn btn-outline-danger btn-sm">Eliminar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
--- panel/resources/views/paneltemplate.blade.php
+++ panel/resources/views/paneltemplate.blade.php
@@ -5,36 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style>
         body {
-            background-color: #faf0ed;
+            background-color: #fcfcfcde;
+            font-family: 'Lato', sans-serif;
         }
 
         /* Sidebar */
         .sidebar ul {
             list-style: none;
-            border: 2px solid red;
             margin: 0;
             padding: 0;
             transition: background-color 0.3s ease-in-out;
         }
 
-        .sidebar ul.active {
-            background-color: rgba(255, 0, 0, 0.3); /* Color rojo cuando activo */
+
+        .sidebar > ul.active > li >a{
+            color: #D2665A;
         }
 
-        .sidebar ul li > a {
-            border: 1px solid blue;
+        .sidebar > ul > li > ul{
+            border: 1 px solid red;
+        }
+
+        .sidebar ul li a {
             display: flex;
             gap: 0.5rem;
-            padding: 0.2rem 0.3rem;
+            padding: 0.3rem 0;
             align-items: center;
             text-decoration: none;
             color: #6d6d6d;
             transition: all .2s;
             cursor: pointer;
+            font-weight: 600;
         }
 
         .sidebar ul li > a:hover {
@@ -49,10 +55,18 @@
         /* Submenú con animación */
         .submenu {
             list-style: none;
-            padding-left: 1.5rem;
+            padding-left: 2.5rem; /* Aumenta este valor para más indentación */
             max-height: 0;
             overflow: hidden;
             transition: max-height 0.5s ease-in-out;
+        }
+
+        .submenu li {
+            padding-left: 1.55rem; /* Ajusta según sea necesario */
+        }
+
+        .submenu li a{
+            font-weight: 500 !important;
         }
 
         .submenu.active {
@@ -63,13 +77,25 @@
         .dropdown.rotate {
             transform: rotate(180deg);
         }
+
+        .shadow-custom {
+            box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.15); /* Sombra menos difuminada */
+            border:0;
+        }
+
+        .action-btn{
+            display:flex; 
+            align-items: center; 
+            gap:0.5rem;
+        }
+
     </style>
 </head>
 <body>
     <div class="container" style="padding-top: 1em">
         <div class="row" style="margin-bottom: 1em">
-            <div class="col-md-3">
-                <img src="{{ asset('img/Logo.png') }}" class="img-fluid" alt="Imagen" width="100">
+            <div class="col-md-2">
+                <img src="{{ asset('img/Logo.png') }}" class="img-fluid" alt="Imagen" width="120">
             </div>
         </div>
 
@@ -79,40 +105,56 @@
                     <ul>
                         <li>
                             <a href="#" class="toggle-submenu">
-                                <i class="fa-solid fa-heart"></i>
-                                <span>Holi</span>
+                                <i class="fa-solid fa-city"></i>
+                                <span>Estudios</span>
                                 <i class="fa-solid fa-chevron-down dropdown"></i>
                             </a>
                             <ul class="submenu">
-                                <li><a href="#">Hola</a></li>
-                                <li><a href="#">Hola</a></li>
+                                <li><a href="#">Crear nuevo</a></li>
+                                <li><a href="#">Ver listado</a></li>
                             </ul>
                         </li>
                     </ul>
                     <ul>
                         <li>
                             <a href="#" class="toggle-submenu">
-                                <i class="fa-solid fa-heart"></i>
-                                <span>Holi 2</span>
+                                <i class="fa-solid fa-users"></i>
+                                <span>Modelos</span>
                                 <i class="fa-solid fa-chevron-down dropdown"></i>
                             </a>
                             <ul class="submenu">
-                                <li><a href="#">Adiós</a></li>
-                                <li><a href="#">Chao</a></li>
+                                <li><a href="#">Crear nuevo</a></li>
+                                <li><a href="#">Ver listado</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a href="#" class="toggle-submenu">
+                                <i class="fa-solid fa-laptop"></i>
+                                <span>Máquinas</span>
+                                <i class="fa-solid fa-chevron-down dropdown"></i>
+                            </a>
+                            <ul class="submenu">
+                                <li><a href="#">Crear nuevo</a></li>
+                                <li><a href="#">Ver listado</a></li>
                             </ul>
                         </li>
                     </ul>
                     <ul>
                         <li>
                             <a href="#">
-                                <i class="fa-solid fa-heart"></i>
-                                <span>Holi 3</span>
+                                <i class="fa-solid fa-right-from-bracket"></i>
+                                <span>Salir del panel</span>
                             </a>
                         </li>
                     </ul>
                 </nav>
             </div>
-        </div>
+            <div class="col md-10 ps-5">
+                @yield("contenido")
+            </div>
+            </div>
     </div>
 
     <script>
