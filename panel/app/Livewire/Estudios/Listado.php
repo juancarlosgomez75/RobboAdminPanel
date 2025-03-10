5d9526e5f9bcce5d334d698d41463b2c26abe99a
Daenloye <ldanielenrique@gmail.com>
2025-03-10 08:57:17 -0500

    Se quita la paginacion

--- panel/app/Livewire/Estudios/Listado.php
+++ panel/app/Livewire/Estudios/Listado.php
@@ -3,7 +3,6 @@
 namespace App\Livewire\Estudios;
 
 use Livewire\Component;
-use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
 class Listado extends Component
@@ -12,16 +11,17 @@ class Listado extends Component
     public $filtroCiudad = "";
     public $filtrosActivos = false;
     public $datos;
-    public $perPage = 2; // Número de elementos por página
 
     public function switchFiltros()
     {
         $this->filtrosActivos = !$this->filtrosActivos;
+        
         if (!$this->filtrosActivos) {
             $this->filtroNombre = "";
             $this->filtroCiudad = "";
         }
     }
+    
 
     public function mount($datos)
     {
@@ -37,32 +37,11 @@ class Listado extends Component
             return $nombreCoincide && $ciudadCoincide;
         });
 
-        // Convertir en colección para paginar
-        $collection = new Collection(array_values($filtrados));
-        $currentPage = request()->query('page', 1); // Página actual
-
-        // Paginar los datos filtrados
-        $paginator = new LengthAwarePaginator(
-            $collection->forPage($currentPage, $this->perPage), // Datos actuales
-            $collection->count(), // Total de datos
-            $this->perPage, // Elementos por página
-            $currentPage, // Página actual
-            ['path' => request()->url(), 'query' => request()->query()] // Mantener query params
-        );
-
-        if (!$this->filtrosActivos) {
-            return view('livewire.estudios.listado', [
-                'texto' => $this->filtroNombre,
-                'filtroOn' => $this->filtrosActivos,
-                'datosUsar' => $paginator,
-            ]);
-        }else{
-            return view('livewire.estudios.listado', [
-                'texto' => $this->filtroNombre,
-                'filtroOn' => $this->filtrosActivos,
-                'datosUsar' => $filtrados
-            ]);
-        }
+        return view('livewire.estudios.listado', [
+            'texto' => $this->filtroNombre,
+            'filtroOn' => $this->filtrosActivos,
+            'datosUsar' => $filtrados
+        ]);
 
     }
 }
