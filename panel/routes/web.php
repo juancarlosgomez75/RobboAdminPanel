390f2c409a7ea5478c84cfa1fb0815f05b966261
Daenloye <ldanielenrique@gmail.com>
2025-03-14 16:34:37 -0500

    Se añade panel a las rutas

--- panel/routes/web.php
+++ panel/routes/web.php
@@ -25,29 +25,29 @@ Route::controller(LoginController::class)->group(function(){
 });
 
 Route::middleware(['auth','checkuserstatus'])->controller(StudyController::class)->group(function () {
-    Route::get('/estudios', 'index')->name('estudios.index');
-    Route::get('/estudios/crear', 'create')->name('estudios.create');
+    Route::get('/panel/estudios', 'index')->name('estudios.index');
+    Route::get('/panel/estudios/crear', 'create')->name('estudios.create');
     // Route::middleware('checkrank:2')->get('/estudios/crear', 'create')->name('estudios.create');
 
-    Route::get('/estudio/manager/crear/{idestudio}', 'manager_create')->name('manager.create');
-    Route::get('/estudio/manager/{idmanager}', 'manager_viewedit')->name('manager.ver');
+    Route::get('/panel/estudio/manager/crear/{idestudio}', 'manager_create')->name('manager.create');
+    Route::get('/panel/estudio/manager/{idmanager}', 'manager_viewedit')->name('manager.ver');
     
-    Route::get('/estudio/{idestudio}', 'viewedit')->name('estudio.ver');
+    Route::get('/panel/estudio/{idestudio}', 'viewedit')->name('estudio.ver');
 });
 
 Route::middleware(['auth','checkuserstatus'])->controller(MachineController::class)->group(function(){
-    Route::get('/maquinas', 'index')->name('maquinas.index');
-    Route::get('/maquinas/crear', 'create')->name('maquinas.create');
+    Route::get('/panel/maquinas', 'index')->name('maquinas.index');
+    Route::get('/panel/maquinas/crear', 'create')->name('maquinas.create');
 });
 
 Route::middleware(['auth','checkuserstatus'])->controller(ModelController::class)->group(function(){
-    Route::get('/modelo/{idmodelo}', 'viewedit')->name('modelos.viewedit');
+    Route::get('/panel/modelo/{idmodelo}', 'viewedit')->name('modelos.viewedit');
 });
 
 Route::middleware(['auth','checkuserstatus'])->controller(AdminController::class)->group(function(){
-    Route::middleware('checkrank:4')->get('/cuentas', 'accounts')->name('admin.accounts');
-    Route::middleware('checkrank:4')->get('/cuenta/{idcuenta}', 'account')->name('admin.account.view');
-    Route::middleware('checkrank:4')->get('/logs', 'logs')->name('admin.logs');
+    Route::middleware('checkrank:4')->get('/panel/cuentas', 'accounts')->name('admin.accounts');
+    Route::middleware('checkrank:4')->get('/panel/cuenta/{idcuenta}', 'account')->name('admin.account.view');
+    Route::middleware('checkrank:4')->get('/panel/logs', 'logs')->name('admin.logs');
 });
 
 // Route::view('dashboard', 'dashboard')
