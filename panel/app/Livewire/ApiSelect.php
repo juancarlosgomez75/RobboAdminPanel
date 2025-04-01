7878edd6703f77ddc429bcb905385e14bef8da26
Daenloye <ldanielenrique@gmail.com>
2025-04-01 15:41:38 -0500

    Se ajusta el slider de entorno de desarrollo

--- panel/app/Livewire/ApiSelect.php
+++ panel/app/Livewire/ApiSelect.php
@@ -6,20 +6,24 @@ use Livewire\Component;
 
 class ApiSelect extends Component
 {
-    public $environment = 'dev'; // Estado inicial
+    public $environment; // Estado inicial
 
     public function updatedEnvironment($value)
     {
         // Aquí puedes manejar la lógica cuando se actualiza el valor
         // Por ejemplo, emitir un evento o cambiar una configuración
-        if($value=="dev"){
+        if($value=="development"){
             session(['API_used' => 'development']);
             $this->dispatch('mostrarToast', 'Cambiar entorno', 'Ahora estás trabajando en desarrollo');
         }
-        elseif($value== 'prod'){
+        elseif($value== 'production'){
             session(['API_used' => 'production']);
             $this->dispatch('mostrarToast', 'Cambiar entorno', 'Ahora estás trabajando en producción');
         }
+    }
+
+    public function mount(){
+        $this->environment = session('API_used',"development");
     }
 
     public function render()
--- panel/resources/views/livewire/api-select.blade.php
+++ panel/resources/views/livewire/api-select.blade.php
@@ -1,7 +1,7 @@
 <div class="btn-group btn-group-sm" role="group" aria-label="Basic radio toggle button group" style="margin-left: 0.35rem;">
-    <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" value="dev" wire:model.live="environment" @if(session('API_used')=="development") checked @endif>
+    <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" value="development" wire:model.live="environment">
     <label class="btn btn-outline-secondary btn-sm" for="btnradio1">Dev</label>
 
-    <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" value="prod" wire:model.live="environment" @if(session('API_used')=="production") checked @endif>
+    <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" value="production" wire:model.live="environment">
     <label class="btn btn-outline-secondary btn-sm" for="btnradio2">Prod</label>
 </div>
