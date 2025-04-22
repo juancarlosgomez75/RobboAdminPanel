0febf83a229858dcef56d46bed1a91dd907ac044
Daenloye <ldanielenrique@gmail.com>
2025-04-22 09:40:19 -0500

    Se añade el can developt al migration

--- panel/database/migrations/0001_01_01_000001_create_users_table.php
+++ panel/database/migrations/0001_01_01_000001_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             
             $table->string('api_token')->nullable();
             $table->boolean('activo')->default(true);
+            $table->boolean('can_developt')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
