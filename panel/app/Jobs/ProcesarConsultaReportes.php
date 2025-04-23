3500487fe2df4cb6eeea51f8a18425c11680172f
Daenloye <ldanielenrique@gmail.com>
2025-04-23 13:17:32 -0500

    Ya obtiene registros

--- panel/app/Jobs/ProcesarConsultaReportes.php
+++ panel/app/Jobs/ProcesarConsultaReportes.php
@@ -9,6 +9,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Auth;
 
 class ProcesarConsultaReportes implements ShouldQueue
 {
@@ -16,11 +17,12 @@ class ProcesarConsultaReportes implements ShouldQueue
 
     //Variables que usaré
     public $studies;
+    public $fechaInicio;
+    public $fechaFin;
     public $tipo;
-    public function __construct($studies,$tipo)
+    public function __construct($studies)
     {
         $this->studies=$studies;
-        $this->tipo=$tipo;
     }
 
     /**
@@ -28,6 +30,107 @@ class ProcesarConsultaReportes implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $total = count($this->studies);
+
+        foreach($this->studies as $index =>$study){
+            //Cargo la info cruda
+            $raw=$study["Data"]["DetailedReport"];
+            $resultado_estudio=[];
+
+            if(!empty($raw)){
+                //Defino los que almacena
+                $resultado_estudio=[
+                    "Acciones"=>[],
+                    "Paginas"=>[],
+                    "Modelos"=>[],
+                    "Tokens"=>0
+                ];
+                foreach($raw as $log){
+                    $modelo=$log["ModelData"]["ModelUserName"];
+                    //Analiso si ya tengo registrado al modelo o no
+                    if(!array_key_exists($modelo, $resultado_estudio["Modelos"])){
+                        $resultado_estudio["Modelos"][$modelo]=[
+                            "Acciones"=>[],
+                            "Paginas"=>[],
+                            "Tokens"=>0
+                        ];
+                    }
+
+                    //Analizo las acciiones generales, si no existte la creo, y si no le sumo
+                    $accion=$log["Action"];
+                    if(!array_key_exists($accion, $resultado_estudio["Acciones"])){
+                        $resultado_estudio["Acciones"][$accion]=[
+                            "Cantidad"=>0,
+                            "Tiempo"=>0
+                        ];
+                    }
+                    if(!array_key_exists($accion, $resultado_estudio["Modelos"][$modelo]["Acciones"])){
+                        $resultado_estudio["Modelos"][$modelo]["Acciones"][$accion]=[
+                            "Cantidad"=>0,
+                            "Tiempo"=>0
+                        ];
+                    }
+
+                    //Ahora aumento en acciones
+                    $resultado_estudio["Acciones"][$accion]["Cantidad"]+=1;
+                    $resultado_estudio["Acciones"][$accion]["Tiempo"]+=$log["ContratedValue"];
+
+                    $resultado_estudio["Modelos"][$modelo]["Acciones"][$accion]["Cantidad"]+=1;
+                    $resultado_estudio["Modelos"][$modelo]["Acciones"][$accion]["Tiempo"]+=$log["ContratedValue"];
+
+                    //Ahora a las páginas, inicio preguntando si es simulador o cual
+                    $pagina = empty($log["Page"]) ? "SIMULADOR" : $log["Page"];
+                    if(!array_key_exists($pagina , $resultado_estudio["Paginas"])){
+                        $resultado_estudio["Paginas"][$pagina]=[
+                            "Tokens"=>0,
+                            "Tipers"=>[]
+                        ];
+                    }
+                    if(!array_key_exists($pagina , $resultado_estudio["Modelos"][$modelo]["Paginas"])){
+                        $resultado_estudio["Modelos"][$modelo]["Paginas"][$pagina]=[
+                            "Tokens"=>0,
+                            "Tipers"=>[]
+                        ];
+                    }
+
+                    //Ahora registro los tokens al total
+                    $resultado_estudio["Tokens"]+=$log["Tokens"];
+                    $resultado_estudio["Modelos"][$modelo]["Tokens"]+=$log["Tokens"];
+
+                    //Ahora por pagina
+                    $resultado_estudio["Paginas"][$pagina]["Tokens"]+=$log["Tokens"];
+                    $resultado_estudio["Modelos"][$modelo]["Paginas"][$pagina]["Tokens"]+=$log["Tokens"];
+
+                    //Ahora los tipers
+                    $tiper = $log["Typer"];
+                    if(!array_key_exists($tiper , $resultado_estudio["Paginas"][$pagina]["Tipers"])){
+                        $resultado_estudio["Paginas"][$pagina]["Tipers"][$tiper]=[
+                            "Tokens"=>0
+                        ];
+                    }
+                    if(!array_key_exists($tiper , $resultado_estudio["Modelos"][$modelo]["Paginas"][$pagina]["Tipers"])){
+                        $resultado_estudio["Modelos"][$modelo]["Paginas"][$pagina]["Tipers"][$tiper]=[
+                            "Tokens"=>0
+                        ];
+                    }
+
+                    //Sumo
+                    $resultado_estudio["Paginas"][$pagina]["Tipers"][$tiper]["Tokens"]+=$log["Tokens"];
+                    $resultado_estudio["Modelos"][$modelo]["Paginas"][$pagina]["Tipers"][$tiper]["Tokens"]+=$log["Tokens"];
+
+
+                }
+            }
+
+            //Ahora modifico
+            unset($this->studies[$study]["Data"]["DetailedReport"]);
+            $this->studies[$study]["Data"]["ResultsReport"]=$resultado_estudio;
+
+            // Actualiza la barra de progreso (ej: en caché)
+            Cache::put("reportProgress_".Auth::user()->id, round((($index + 1) / $total) * 100), 600);
+        }
+
+        // Guarda los resultados finales
+        Cache::put("reportResult_".Auth::user()->id, $this->studies, 600);
     }
 }
--- panel/app/Livewire/Estudios/Reporte.php
+++ panel/app/Livewire/Estudios/Reporte.php
@@ -40,8 +40,107 @@ class Reporte extends Component
                 ]
             ]
         ];
-        // $informacion_logs=sendBack($data_send);
-        $this->resultado=sendBackPython($data_send);
+        //Obtengo la informacion que requiero
+        $data=sendBack($data_send);
+
+        //Analizo si el status es true
+        if($data["Status"]??False){
+            if($data["Data"]["DetailedReport"]?? False){
+                $raw=$data["Data"]["DetailedReport"];
+                if(!empty($raw)){
+                    //Defino los que almacena
+                    $this->resultado=[
+                        "Acciones"=>[],
+                        "Paginas"=>[],
+                        "Modelos"=>[],
+                        "Tokens"=>0
+                    ];
+                    foreach($raw as $log){
+                        $modelo=$log["ModelData"]["ModelUserName"];
+                        //Analiso si ya tengo registrado al modelo o no
+                        if(!array_key_exists($modelo, $this->resultado["Modelos"])){
+                            $this->resultado["Modelos"][$modelo]=[
+                                "Acciones"=>[],
+                                "Paginas"=>[],
+                                "Tokens"=>0
+                            ];
+                        }
+
+                        //Analizo las acciiones generales, si no existte la creo, y si no le sumo
+                        $accion=$log["Action"];
+                        if(!array_key_exists($accion, $this->resultado["Acciones"])){
+                            $this->resultado["Acciones"][$accion]=[
+                                "Cantidad"=>0,
+                                "Tiempo"=>0
+                            ];
+                        }
+                        if(!array_key_exists($accion, $this->resultado["Modelos"][$modelo]["Acciones"])){
+                            $this->resultado["Modelos"][$modelo]["Acciones"][$accion]=[
+                                "Cantidad"=>0,
+                                "Tiempo"=>0
+                            ];
+                        }
+
+                        //Ahora aumento en acciones
+                        $this->resultado["Acciones"][$accion]["Cantidad"]+=1;
+                        $this->resultado["Acciones"][$accion]["Tiempo"]+=$log["ContratedValue"];
+
+                        $this->resultado["Modelos"][$modelo]["Acciones"][$accion]["Cantidad"]+=1;
+                        $this->resultado["Modelos"][$modelo]["Acciones"][$accion]["Tiempo"]+=$log["ContratedValue"];
+
+                        //Ahora a las páginas, inicio preguntando si es simulador o cual
+                        $pagina = empty($log["Page"]) ? "SIMULADOR" : $log["Page"];
+                        if(!array_key_exists($pagina , $this->resultado["Paginas"])){
+                            $this->resultado["Paginas"][$pagina]=[
+                                "Tokens"=>0,
+                                "Tipers"=>[]
+                            ];
+                        }
+                        if(!array_key_exists($pagina , $this->resultado["Modelos"][$modelo]["Paginas"])){
+                            $this->resultado["Modelos"][$modelo]["Paginas"][$pagina]=[
+                                "Tokens"=>0,
+                                "Tipers"=>[]
+                            ];
+                        }
+
+                        //Ahora registro los tokens al total
+                        $this->resultado["Tokens"]+=$log["Tokens"];
+                        $this->resultado["Modelos"][$modelo]["Tokens"]+=$log["Tokens"];
+
+                        //Ahora por pagina
+                        $this->resultado["Paginas"][$pagina]["Tokens"]+=$log["Tokens"];
+                        $this->resultado["Modelos"][$modelo]["Paginas"][$pagina]["Tokens"]+=$log["Tokens"];
+
+                        //Ahora los tipers
+                        $tiper = $log["Typer"];
+                        if(!array_key_exists($tiper , $this->resultado["Paginas"][$pagina]["Tipers"])){
+                            $this->resultado["Paginas"][$pagina]["Tipers"][$tiper]=[
+                                "Tokens"=>0
+                            ];
+                        }
+                        if(!array_key_exists($tiper , $this->resultado["Modelos"][$modelo]["Paginas"][$pagina]["Tipers"])){
+                            $this->resultado["Modelos"][$modelo]["Paginas"][$pagina]["Tipers"][$tiper]=[
+                                "Tokens"=>0
+                            ];
+                        }
+
+                        //Sumo
+                        $this->resultado["Paginas"][$pagina]["Tipers"][$tiper]["Tokens"]+=$log["Tokens"];
+                        $this->resultado["Modelos"][$modelo]["Paginas"][$pagina]["Tipers"][$tiper]["Tokens"]+=$log["Tokens"];
+
+
+                    }
+                }
+
+            }
+
+            // //Ahora envio a Python a procesarla
+            //     $this->resultado=sendBackPython([
+            //         "Action"=>"ProcessData",
+            //         "Data"=>$data["Data"]["DetailedReport"]
+            //     ]);
+        }
+
 
     }
 
