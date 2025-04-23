9de2d7ae0bd9121e1cccd2f804b26ac31330ec8c
Daenloye <ldanielenrique@gmail.com>
2025-04-23 14:58:47 -0500

    Ya acepta funciones distintas la barra

--- panel/app/Livewire/Progressbar.php
+++ panel/app/Livewire/Progressbar.php
@@ -9,18 +9,24 @@ class Progressbar extends Component
 {
     public $userId;
     public $functionId;
+    public $endSignal;
     public $progreso = 0;
 
-    public function mount($userId,$functionId){
+    public function mount($userId,$functionId,$endSignal){
         $this->userId=$userId;
         $this->functionId=$functionId;
+        $this->endSignal=$endSignal;
     }
     public function render()
     {
         $this->progreso = Cache::get($this->functionId."_" . $this->userId, 0);
 
         if($this->progreso==100){
-            $this->dispatch('progressDone');
+            //Indico que ya terminé
+            $this->dispatch($this->endSignal);
+
+            //Reinicio la variable
+            Cache::forget($this->functionId."_" . $this->userId);
         }
 
         return view('livewire.progressbar');
