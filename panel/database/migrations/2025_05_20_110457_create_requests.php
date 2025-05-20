33507956428bf7ff1f0ae850c177f7976bab4693
Daenloye <ldanielenrique@gmail.com>
2025-05-20 11:39:46 -0500

    Se crea la migración de requests

---
+++ panel/database/migrations/2025_05_20_110457_create_requests.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+
+            //Información del contratista
+            $table->string('name');
+            $table->string('city')->nullable();;
+            $table->string('address')->nullable();;
+            $table->string('phone')->nullable();;
+
+            //Información de creación
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->text('creation_notes')->nullable();
+            $table->json("creation_list");
+            $table->timestamp('tentative_delivery_date')->nullable();
+
+            //Información de entrega
+            $table->json("pending_list");
+            $table->json("delivery_list")->nullable();
+
+            //Información de estado
+            $table->enum('status', ['created', 'partial delivery', 'delivered', 'inventoried', 'canceled'])->default('created');
+            
+
+            //Cancelación
+            $table->unsignedBigInteger('inventoried_by')->nullable();
+            $table->foreign('inventoried_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->text('inventoried_notes')->nullable();
+            $table->timestamp('inventoried_date')->nullable();
+
+            //Inventarialización
+            $table->unsignedBigInteger('canceled_by')->nullable();
+            $table->foreign('canceled_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->text('cancellation_reason')->nullable();
+            $table->timestamp('cancel_date')->nullable();
+
+            //Marcas temporales
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
