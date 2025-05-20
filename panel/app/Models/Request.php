14a8e9b5c6627e486c5fa9679c1a9c814af20be6
Daenloye <ldanielenrique@gmail.com>
2025-05-20 11:41:33 -0500

    Se crea el modelo de request

---
+++ panel/app/Models/Request.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Request extends Model
+{
+    protected $table = 'requests';
+
+    public function creator_info()
+    {
+        return $this->belongsTo(User::class, 'created_by', 'id');
+    }
+}
